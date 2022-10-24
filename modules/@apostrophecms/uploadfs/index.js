9e4c3f3cecb5d09c63a747cd492ab9dd5149a30f
Noah Gysin <noah@wochen-zeitung.ch>
2022-10-24 09:04:52 +0200

    added uploadfs config

---
+++ modules/@apostrophecms/uploadfs/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  options: {
+    uploadfs: {
+      storage: 's3',
+      secret: '9ZkaALlpDKNTwQh+1dPAG37934/Bk8DjSWCiVAJR',
+      key: 'AKIAVCFHHYNAIUGBOSMV',
+      bucket: 'test-apos',
+      region: 'eu-central-1',
+      https: true
+    },
+  },
+};
